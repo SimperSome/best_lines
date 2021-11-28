7474c717606e96add737a628e632217d8974b7cf
anonymous anonymous <eswarpradyu@gmail.com>
2021-11-28 23:18:02 +0000

    Generate graphiti resources for books

---
+++ app/resources/book_resource.rb
@@ -0,0 +1,13 @@
+class BookResource < ApplicationResource
+  attribute :id, :integer, writable: false
+  attribute :created_at, :datetime, writable: false
+  attribute :updated_at, :datetime, writable: false
+  attribute :name, :string
+  attribute :publishing_year, :integer
+  attribute :author_id, :integer
+
+  # Direct associations
+
+  # Indirect associations
+
+end
